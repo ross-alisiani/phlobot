8d0dcc68d99f36ae3385c78e7f30d88089163196
ross-alisiani <ross.phlobot@gmail.com>
2026-04-12 08:56:16 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -12,6 +12,7 @@ export async function middleware(request: NextRequest) {
         getAll() { return request.cookies.getAll(); },
         setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value }) =>
+            
             request.cookies.set(name, value)
           );
           supabaseResponse = NextResponse.next({ request });
