206853de091cb0ca6dd22418085a2de89daf329a
ross-alisiani <ross.phlobot@gmail.com>
2026-05-03 17:00:05 -0600

    Create sentry.server.config.ts

---
+++ sentry.server.config.ts
@@ -0,0 +1,13 @@
+// This file configures the initialization of Sentry on the server side.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+  // Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1.0,
+
+  debug: false,
+});
